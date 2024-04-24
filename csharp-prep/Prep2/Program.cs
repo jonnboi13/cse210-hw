5282374cd3e57db6ad6322071c2a591e4cb41e22
jonnboi13 <jonnyboi13@byui.edu>
2024-04-24 17:28:08 -0600

    Completed prep 1 and 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,48 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade?");
+        string userInput = Console.ReadLine();
+        int grade = int.Parse(userInput);
+
+        string letter = "";
+
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+
+        else if (grade < 60)
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your letter grade is {letter}.");
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("You passed!");
+        }
+        else
+        {
+            Console.WriteLine("Nice try... Better luck next time!");
+        }
+
     }
+
 }
