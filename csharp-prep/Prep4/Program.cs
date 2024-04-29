0595fe4d89cf1ae237f3c41a405c776015738e99
jonnboi13 <jonnyboi13@byui.edu>
2024-04-29 11:22:23 -0600

    prep 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,49 @@
 using System;
+using System.Collections.Generic;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        int userNumber = -1;
+        List<int> numbers = new List<int>();
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        while (userNumber != 0)
+        {
+            Console.Write("Ender a number: ");
+            userNumber = int.Parse(Console.ReadLine());
+            if (userNumber != 0)
+            {
+                numbers.Add(userNumber);
+            }
+        }
+        Console.Write(numbers);
+
+        // Find the sum
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+        Console.WriteLine($"The sum is {sum}.");
+
+        // Find the average
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average is {average}.");
+
+        // Find the highest number
+        int maxNumber = 0;
+        foreach (int number in numbers)
+        {
+            if (number > maxNumber)
+            {
+                maxNumber = number;
+            }
+        }
+        Console.WriteLine($"The highest number is {maxNumber}.");
+
+
     }
 }
