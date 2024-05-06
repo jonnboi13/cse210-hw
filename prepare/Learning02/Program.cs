3dbee8fc139e64a506b1c101cba63dfd6d014dcb
jonnboi13 <jonnyboi13@byui.edu>
2024-05-06 11:25:03 -0600

    Learning Activity 2

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,32 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+
+        Job Job1 = new Job();
+        Job1._jobTitle = "Software Engineer";
+        Job1._company = "Microsoft";
+        Job1._startYear = "2019";
+        Job1._endYear = "2022";
+
+
+        Job Job2 = new Job();
+        Job2._jobTitle = "Manager";
+        Job2._company = "Apple";
+        Job2._startYear = "2022";
+        Job2._endYear = "2023";
+
+
+        Resume myResume = new Resume();
+        myResume._name = "Jonathan Oliphant";
+        myResume._jobs.Add(Job1);
+        myResume._jobs.Add(Job2);
+
+
+        myResume.DisplayResume();
+
     }
 }
