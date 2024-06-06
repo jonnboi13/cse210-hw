3624520101aa10f5634aeb287fbb1670ef20ba04
jonnboi13 <jonnyboi13@byui.edu>
2024-06-05 18:29:17 -0600

    fixed the journal.cs file to allow for saving default files

--- prove/Develop02/Journal.cs
+++ prove/Develop02/Journal.cs
@@ -3,21 +3,17 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using Newtonsoft.Json;
-
 public class Journal
 {
     public List<Entry> Entries { get; set; }
-
     public Journal()
     {
         Entries = new List<Entry>();
     }
-
     public void AddEntry(string content, string prompt)
     {
         Entries.Add(new Entry(content, prompt));
     }
-
     public void DisplayEntries()
     {
         foreach (var entry in Entries)
@@ -28,7 +24,6 @@ public class Journal
             Console.WriteLine("--------------------------------------------------");
         }
     }
-
     public void SaveToFile(string filename)
     {
         try
@@ -39,35 +34,33 @@ public class Journal
             {
                 // If it's a JSON file, serialize the Entries list to JSON
                 string json = JsonConvert.SerializeObject(Entries);
-
-                // Write the JSON string to the file
                 File.WriteAllText(filename, json);
-            }
-            else if (extension == ".txt")
-            {
-                // If it's a text file, write each entry's content to the file
-                File.WriteAllLines(filename, Entries.Select(entry => entry.Content));
             }
             else
             {
-                // If it's neither, print an error message
-                Console.WriteLine($"Unsupported file type: {extension}");
+                // For other file types, save as comma-separated values
+                using (StreamWriter sw = new StreamWriter(filename))
+                {
+                    foreach (var entry in Entries)
+                    {
+                        sw.WriteLine($"{entry.Prompt},{entry.Content},{entry.Date.ToString("MM/dd/yyyy h:mm:ss tt")}");
+                    }
+                }
             }
         }
         catch (Exception ex)
         {
-            // If there's an error, print an error message
-            Console.WriteLine($"Error writing data to file: {filename}. Error: {ex.Message}");
+            Console.WriteLine($"An error occurred: {ex.Message}");
         }
     }
-
     public void LoadFromFile(string filename)
     {
         try
         {
+            // Clear the current entries
+            Entries.Clear();
             // Read the file content
             string[] lines = File.ReadAllLines(filename);
-
             // Check the file extension
             string extension = Path.GetExtension(filename);
             if (extension == ".json")
@@ -75,7 +68,7 @@ public class Journal
                 // If it's a JSON file, deserialize the content to a list of Entry objects
                 Entries = JsonConvert.DeserializeObject<List<Entry>>(string.Join("", lines));
             }
-            else if (extension == ".txt")
+            else
             {
                 // If it's a text file, parse each line into a new Entry and add it to the list
                 foreach (string line in lines)
@@ -92,14 +85,11 @@ public class Journal
                     }
                 }
             }
-            else
-            {
-                // If it's neither, print an error message
-                Console.WriteLine($"Unsupported file type: {extension}");
-            }
+
         }
         catch (FileNotFoundException)
         {
+
             // If the file doesn't exist, print an error message
             Console.WriteLine($"File not found: {filename}");
         }
@@ -107,6 +97,11 @@ public class Journal
         {
             // If there's an error in the JSON data, print an error message
             Console.WriteLine($"Error reading data from file: {filename}");
+        }
+        catch (ArgumentException)
+        {
+            // If the filename is empty, print an error message
+            Console.WriteLine("Filename cannot be empty.");
         }
     }
 }
